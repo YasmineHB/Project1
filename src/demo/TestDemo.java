4961c1b8f710f1d9f082a04bd7878259cde09221
aaldu <aaldu@DESKTOP-26OB3J1.myfiosgateway.com>
2024-01-12 18:46:21 -0500

    yes

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -5,18 +5,21 @@ public class TestDemo {
 	// Name: --> Yasmine ...
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("HELLO GITHUB...");
 
 		System.out.println("HELLO FROM GIT TO ECLIPSE");
-		
+
 		System.out.println("HOW ARE YOU GITHUB?");
+
+		System.out.println("PRACTICE");
+
+		System.out.println("***HELLOO *** t4");
 	}
 
-	
-	//Add a comment to test...
+	// Add a comment to test...
 
-	//this is coming from GitHub...
+	// this is coming from GitHub...
 
 	// this comment from GitHb T4...
 
@@ -27,6 +30,7 @@ public class TestDemo {
 	//hello
 	//hello
 	//hiii
+
 
 
 }
